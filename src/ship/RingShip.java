aa1172db6f719ac65c5e3380a8d5499b15a5343f
forbesb <magnos.cartus@gmail.com>
2015-01-17 14:51:56 -0500

    Pursuit works
    Now the ships are really hard to dodge

--- src/ship/RingShip.java
+++ src/ship/RingShip.java
@@ -3,21 +3,78 @@ package ship;
 import java.awt.Color;
 import java.awt.Graphics;
 import java.util.ArrayList;
+import java.util.Vector;
 
 import ship.weapons.Weapon;
 import ship.weapons.ammo.Bullet;
 
 public class RingShip extends Enemy{
+	private double tyo=0, txo=0, tyn=0, txn=0;
+	
 	public RingShip(double x, double y){
 		super(x, y);
-		speed=1;
+		speed=3;
 		health=1;
 	}
 	@Override
 	public void target(Ship user){
-		tx=user.getX();
-		ty=user.getY();
+		tyo = tyn;
+		txo = txn;
+		tyn = user.getY();
+		txn = user.getX();
+		//System.out.println("Old:"+txo+","+tyo);
+		//System.out.println("New:"+txn+","+tyn);
+		
+		
+		pursuit();
 	}
+	private void pursuit(){
+		Vector<Double> targetVector = getVector();
+		//T= Dc
+		//idk how c works, turning parameter apparently? I took rad/frame divided by accel/frame
+		double T = getDistance()*0.5;
+		//double T = 10;
+		double xs = targetVector.get(1);
+		double ys = targetVector.get(2);
+		double fx = txn+xs*T;
+		System.out.println(xs);
+		double fy = tyn+ys*T;
+		System.out.println(ys);
+		tx = fx;
+		ty = fy;
+		System.out.println("Target:" +tx+","+ty);
+		
+		
+		
+	}
+	
+	private Vector<Double> getVector(){
+		Vector<Double> tv = new Vector<Double>();
+		double xspeed = txn-txo;
+		double yspeed = tyn-tyo;
+		double tSpeed = Math.sqrt(Math.pow(xspeed, 2)+ Math.pow(yspeed, 2));
+		tv.add(tSpeed);
+		tv.add(xspeed);
+		tv.add(yspeed);
+		double tAngle = Math.atan(yspeed/xspeed);
+		boolean ypos = yspeed>0;
+		boolean xpos = xspeed>0;
+		if ((ypos && !xpos) || (!ypos&& xpos)){
+			tAngle+=180;
+		}
+		tv.add(tAngle);
+		return tv;
+	}
+	
+	private double getDistance(){
+		
+		double Dx = txn-x;
+		double Dy = tyn-y;
+		double Dt = Math.sqrt(Math.pow(Dx, 2)+Math.pow(Dy, 2));
+		
+		return Dt;
+	}
+	
 	@Override
 	public void avoid(Ship user){
 		
